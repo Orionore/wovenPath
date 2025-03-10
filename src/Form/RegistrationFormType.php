78a0316a0173aa8d708b948cc3801ddb057f71bc
Orionore <lucien.rdl@gmail.com>
2025-03-10 12:51:03 +0100

    fix: email verifier don't get error 500

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\User;
+use App\Validator\UniqueUserEmail;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -10,6 +11,7 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\NotCompromisedPassword;
@@ -30,6 +32,11 @@ class RegistrationFormType extends AbstractType
                 'required' => true,
                 'label' => false,
                 'attr' => ['placeholder' => 'Email'],
+                'constraints' => [
+                    new NotBlank(['message' => 'Veuillez entrer une adresse email']),
+                    new Email(['message' => 'Veuillez entrer une adresse email valide']),
+                    new UniqueUserEmail()
+                ]
             ])
             ->add('password', RepeatedType::class, [
                 'type' => PasswordType::class,
